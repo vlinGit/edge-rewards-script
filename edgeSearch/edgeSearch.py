0e7685da967b7d122a2f00f3e59745fdb6869db0
BuildTools <vlin0812@gmail.com>
2023-08-06 17:56:21 -0400

    Improved searching, added more task capabilities. Quizz completions are hit or miss will need to continue monitoring

--- edgeSearch/edgeSearch.py
+++ edgeSearch/edgeSearch.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
 from pytrends.request import TrendReq
 from keywords import keywords
 import time
@@ -21,31 +23,17 @@ pointsPerSearch = 5
 def login():
     driver.get('https://login.microsoftonline.com/')
 
-#FIXME:
-#   Warpspeed broken
-#   Only does the sequence once
-#   Needs a check to see if question is complete
-#   Need a check for entertainment quiz (7 questions), do a check for #of questions and base for loop off that
-def doQuiz(isWarpspeed):
-    buttonX = 364
-    buttonY = 994
-    
-    if isWarpspeed:
-        time.sleep(3)
-        for i in range(0,3):
-            time.sleep(2)
-            choices = driver.find_elements(By.CLASS_NAME, "rq_button")
-            for choice in choices:
-                try:
-                    driver.execute_script("arguments[0].click()",choice)
-                except Exception:
-                    break
-    else:
-        for i in range(0,3):
-            time.sleep(0.5)
-            driver.find_element(By.XPATH, f"//*[@id='QuestionPane{i}']/div[1]/div[2]/a[1]/div/div").click()
-            time.sleep(0.5)
-            driver.find_element(By.XPATH, f"//*[@id='nextQuestionbtn{i}']").click()
+#TODO:
+#   Need a check for entertainment quiz (7 questions) <- this might be done, but will need to check once the quiz pops up in tasks
+def clickButton(method: By, path):
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((method, path)))
+        button = driver.find_element(method, path)
+        driver.execute_script("arguments[0].click()", button)
+        return True
+    except TimeoutException:
+        print(f"Button could not be found\nMethod: {method}\nPath: {path}\n")
+        return False
 
 # Index when parsed:
 #   0: numeric points rewarded
@@ -58,6 +46,57 @@ def rewardText(rawText):
     
     return text
 
+# Grabs '(x of 10)' and converts to [x, 10]
+def wqIsolateNumbers():
+    panes = driver.find_element(By.XPATH, '//*[@id="ListOfQuestionAndAnswerPanes"]')
+    progress = panes.find_elements(By.CSS_SELECTOR, 'div')
+    extract = progress[-1].get_attribute('innerText')[1:-1].split('of')
+    numbers = [int(extract[0].strip()), int(extract[1].strip())]
+    
+    return numbers
+
+# Warpspeed and supersonic quizzes
+def specialQuiz():
+    try:
+        clickButton(By.XPATH, '//*[@id="rqStartQuiz"]')
+    except NoSuchElementException:
+        print('Element not found, quiz was likely already started')
+    curPoints = int(driver.find_element(By.CLASS_NAME, 'rqECredits').get_attribute('innerText'))
+    maxPoints = int(driver.find_element(By.CLASS_NAME, 'rqMCredits').get_attribute('innerText'))
+    option = 0
+    while curPoints < maxPoints:
+        if clickButton(By.XPATH, f'//*[@id="rqAnswerOption{option}"]'):
+            newPoints = int(driver.find_element(By.CLASS_NAME, 'rqECredits').get_attribute('innerText'))
+            
+            if curPoints != newPoints: 
+                option = 0
+                curPoints = newPoints
+            else:
+                option += 1
+        else:
+            print("Submit button not found in specialQuiz, stopping quiz")
+            break
+
+def abcQuiz():
+    progress = wqIsolateNumbers()
+    curQuestion = progress[0]
+    maxQuestion = progress[1]
+    
+    while curQuestion <= maxQuestion:
+        if clickButton(By.CLASS_NAME, 'wk_choicesInstLink'):
+            time.sleep(2)
+            if clickButton(By.XPATH, f'//*[@id="nextQuestionbtn{curQuestion-1}"]') and curQuestion != maxQuestion:
+                time.sleep(2)
+                updateProgress = wqIsolateNumbers()
+                curQuestion = updateProgress[0]
+            else:
+                print("Submit button not found in abcQuiz, stopping quiz")
+                break
+        else:
+            print("Choice button not found in abcQuiz, stopping quiz")
+            break
+        
+    
 def doDailySets():
     driver.get('https://rewards.bing.com/?signin=1')
     daily = driver.find_elements(By.XPATH, "//*[@id='daily-sets']/mee-card-group[1]/div/mee-card")
@@ -79,12 +118,17 @@ def doDailySets():
         
         if taskClass == 'mee-icon mee-icon-AddMedium': # true if task has not been
             reward.click()
+            time.sleep(1)
             driver.switch_to.window(driver.window_handles[newWindow])
             
-            if 'poll' in text[1]:
-                choice = driver.find_element(By.ID, f'btoption{random.randint(0, 1)}')
-                driver.execute_script("arguments[0].click();", choice)
-
+            if 'warpspeed quiz' in text[1] or 'supersonic quiz' in text[1]:
+                specialQuiz()
+            elif 'poll' in text[1]:
+                if not clickButton(By.ID, f'btoption{random.randint(0, 1)}'):
+                    print("Poll button not found")
+            elif 'test your smarts' in text[1] or 'show what you know' in text[1] or 'a, b, or c' in text[1] or 'quiz' in text[2]:
+                abcQuiz()
+            
             driver.switch_to.window(driver.window_handles[0]) 
             newWindow += 1
         
@@ -98,18 +142,31 @@ def getCap():
     print("Avaliable Points: ", pcCap, mobileCap)
     
     return pcCap, mobileCap
+        
 
-# Grab a random keyword generates a suggested search with pytrends
-# Why this way, IDK
+# Generates search using pytrends
+# def generateString(cap):
+#     pytrends = TrendReq(hl='en-US', tz=360)
+#     searches = []
+
+#     while len(searches) < cap/pointsPerSearch:
+#         suggestions = pytrends.suggestions(keyword=keywords[random.randint(0, len(keywords)-1)])
+#         for suggestion in suggestions:
+#             if (not suggestion["title"] in searches) and len(suggestion["title"]) < 10:
+#                 searches.append(suggestion["title"])
+    
+#     return searches
+
+# Searches using a random keyword
 def generateString(cap):
-    pytrends = TrendReq(hl='en-US', tz=360)
+    #pytrends = TrendReq(hl='en-US', tz=360)
     searches = []
 
     while len(searches) < cap/pointsPerSearch:
-        suggestions = pytrends.suggestions(keyword=keywords[random.randint(0, len(keywords)-1)])
-        for suggestion in suggestions:
-            if (not suggestion["title"] in searches):
-                searches.append(suggestion["title"])
+        #suggestions = pytrends.suggestions(keyword=keywords[random.randint(0, len(keywords)-1)])
+        suggestion = keywords[random.randint(0, len(keywords)-1)]
+        if suggestion not in searches:
+            searches.append(suggestion)
     
     return searches
 
@@ -122,24 +179,36 @@ def searchLoop(cap=0):
     
     points = 0
     searchCounter = 0
-    while points <= cap:
+    while points < cap:
         searchBar = driver.find_element(By.ID, "sb_form_q")
-        searchBar.clear()
+        while searchBar.get_attribute('value') != '':
+            searchBar.clear()
+            time.sleep(0.2)  
         searchBar.send_keys(searches[searchCounter])
         searchBar.submit()
         time.sleep(random.randint(1,3)/10)
         points += pointsPerSearch # += 5
         searchCounter += 1
 
+# hotkeys are for turning mobile mode on and off
+# done by enabling dev console and using device emulation
 def doSearch(pcCap, mobileCap):
     searchLoop(pcCap)
     pyautogui.hotkey('ctrl', 'shift', 'i')
     time.sleep(1)
     pyautogui.hotkey('ctrl', 'shift', 'm')
     searchLoop(mobileCap)
+    pyautogui.hotkey('ctrl', 'shift', 'i')
+    driver.refresh()
     
 if __name__ == "__main__":
     doDailySets()
-    pcCap, mobileCap = getCap()
-    driver.get('https://bing.com')
-    doSearch(pcCap, mobileCap)
+    
+    pcAvailPoints, mobileAvailPoints = getCap()
+    while pcAvailPoints != 0 or mobileAvailPoints != 0:
+        driver.get('https://bing.com')
+        doSearch(pcAvailPoints, mobileAvailPoints)    
+        pcAvailPoints, mobileAvailPoints = getCap()
+        
+    driver.get('https://rewards.bing.com/')
+    driver.find_element(By.XPATH, '//*[@id="dailypointColumnCalltoAction"]').click()
